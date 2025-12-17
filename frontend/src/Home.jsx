bba3bf1e1d239efd55d076b65ea6b3b619fd12dc
harisudharshann2024laiml-ui <harisudharshann@gmail.com>
2025-12-17 09:59:14 +0530

    initial

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home(){
+    return(
+        <div>
+            <h1>Home Page</h1>
+            <p>Welcome to our Simple React store.</p>
+            <a href="./Products.jsx"><img src="./assets/image.png" alt="logo" style={{height:"10px"}}/></a>
+            <p>click the immage for more info </p>
+        </div>
+    )
+}
---
+++ frontend/src/contact.jsx
@@ -0,0 +1,9 @@
+export default function contact() {
+    return (
+        <div style={{padding:"20px"}}>
+            <h1>Contact Us</h1>
+            <p>Email: support@store.com</p>
+            <p>Phone: 987646912</p>
+        </div>
+    );
+}
